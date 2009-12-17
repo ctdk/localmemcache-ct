f5b648f2816472ee8d043acb3c19a14ff4af935b
Jeremy Bingham <jbingham@gmail.com>
2009-12-17 02:09:19 -0800

    POSIX shared mem

--- src/lmc_shm.c
+++ src/lmc_shm.c
@@ -70,7 +70,7 @@ void lmc_shm_ensure_namespace_file(const char *ns) {
   lmc_shm_ensure_root_path();
   char fn[1024];
   lmc_file_path_for_namespace((char *)&fn, ns);
-  if (!lmc_does_namespace_exist(ns)) { close(open(fn, O_CREAT, 0777)); }
+  /* if (!lmc_does_namespace_exist(ns)) { close(open(fn, O_CREAT, 0777)); } */
 }
 
 lmc_shm_t *lmc_shm_create(const char* namespace, size_t size, lmc_error_t *e) {
@@ -82,32 +82,44 @@ lmc_shm_t *lmc_shm_create(const char* namespace, size_t size, lmc_error_t *e) {
   snprintf((char *)&mc->namespace, 1023, "%s", namespace);
   mc->size = size;
 
-  lmc_shm_ensure_namespace_file(mc->namespace);
+  fprintf(stderr, "Marching through shm creation\n");
+
+  /* lmc_shm_ensure_namespace_file(mc->namespace); */
   char fn[1024];
   lmc_file_path_for_namespace((char *)&fn, mc->namespace);
-  if (!lmc_handle_error((mc->fd = open(fn, O_RDWR, (mode_t)0777)) == -1, 
+  fprintf(stderr, "Point 1\n");
+  if (!lmc_handle_error((mc->fd = shm_open(fn, O_RDWR | O_CREAT, (mode_t)0777)) == -1, 
       "open", "ShmError", e)) goto open_failed;
-  if (!lmc_handle_error(lseek(mc->fd, mc->size - 1, SEEK_SET) == -1, 
-      "lseek", "ShmError", e)) goto failed;
+  fprintf(stderr, "Point 2\n");
+  ftruncate(mc->fd, mc->size - 1);
+  fprintf(stderr, "Point 3\n");
+  /* if (!lmc_handle_error(lseek(mc->fd, mc->size - 1, SEEK_SET) == -1, 
+      "lseek", "ShmError", e)) goto failed; */
   if (!lmc_handle_error(write(mc->fd, "", 1) != 1, "write", 
       "ShmError", e)) goto failed;
+  fprintf(stderr, "Point 4\n");
   mc->base = mmap(0, mc->size, PROT_READ | PROT_WRITE, MAP_SHARED, mc->fd, 
       (off_t)0);
+  fprintf(stderr, "Point 5\n");
   if (!lmc_handle_error(mc->base == MAP_FAILED, "mmap", "ShmError", e)) 
      goto failed;
   return mc;
 
 failed:
-  close(mc->fd);
+  shm_unlink(fn);
+  fprintf(stderr, "Failed\n");
 open_failed:
   free(mc);
+  fprintf(stderr, "Open failed :-(\n");
   return NULL;
 }
 
 int lmc_shm_destroy(lmc_shm_t *mc, lmc_error_t *e) {
   int r = lmc_handle_error(munmap(mc->base, mc->size) == -1, "munmap", 
       "ShmError", e);
-  close(mc->fd);
+  char fn[1024];
+  lmc_file_path_for_namespace((char *)&fn, mc->namespace);
+  shm_unlink(fn);
   free(mc);
   return r;
 }
